bf50cf276d319b2930012e7f47b33319b88caf3c
garzaro <clebergarzaro74@gmail.com>
2024-10-31 21:05:49 -0300

    inciando o curso

--- src/main/java/dev/cleber/Main.java
+++ src/main/java/dev/cleber/Main.java
@@ -1,7 +1,14 @@
 package dev.cleber;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        JFrame janela  =new JFrame();
+        janela.setTitle("Teste");
+        janela.setSize(500, 320);
+        janela.setVisible(true);
     }
 }
