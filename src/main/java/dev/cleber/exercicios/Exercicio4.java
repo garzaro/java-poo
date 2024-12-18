f4c9cff19716a79e20cffdce0ee08a3e4b6331a3
garzaro <clebergarzaro74@gmail.com>
2024-12-18 09:54:21 -0300

    exercicio 4

---
+++ src/main/java/dev/cleber/exercicios/Exercicio4.java
@@ -0,0 +1,35 @@
+package dev.cleber.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+	
+	public static void main(String[] args) {
+		
+		/*calcular a elevação de numeros ao quadrado*/
+		
+		int expoente = 2;
+	    
+		int numeroElevadoAoQuadrado = 2;
+
+	    double resultado = Math.pow(numeroElevadoAoQuadrado, expoente);
+	    
+	    System.out.println("O resultado da elevação é " + resultado);
+	    
+	    /*scanner*/
+
+	    Scanner exponenciacao = new Scanner(System.in);
+
+	    System.out.println("Digite um numero.");
+	    int numeroElevado = exponenciacao.nextInt();
+
+	    double resultadoElevado = Math.pow(numeroElevado, expoente);
+
+	    System.out.println("O resultado da elevação é " + resultadoElevado);
+
+	    exponenciacao.close();
+		
+	}	
+}
+
+
